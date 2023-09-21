1e20fa299e983c3c8dd665d941489664b78ac549
Pablo E. Sodo <pablosodo@gmail.com>
2023-09-21 10:45:34 -0300

    general Ch

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 //import 'que quiero importar' from 'donde esta lo que quiero importar'
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 
